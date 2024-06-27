ee937309ba292121a28504e3d11d48b35e495bb1
Anjali Chopra <anjalichopra1994@gmail.com>
2024-06-27 23:47:09 +0530

    add: config file for backend

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://anjalichopra1994:MaKrxYZZKIRxD3SM@book-store.ky6d6wq.mongodb.net/?retryWrites=true&w=majority&appName=book-store';
